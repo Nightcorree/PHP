b451105b3317298f934e6ab3a00f3df9b1ac1f53
Nightcore <Farmax7777@yandex.ru>
2024-02-13 19:33:39 +0300

    Update

---
+++ lab2/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a src="index.html"></a>
+    <?php
+        print_r(get_headers('https://sch2200.mskobr.ru/'))
+    ?>
+</body>
+</html>
